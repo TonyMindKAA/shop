5dbb00ef74588ac2136d76d003e3ce1498f76c07
Anton_Krynytskyi <Anton_Krynytskyi@epam.com>
2015-05-06 14:20:42 +0300

    add login tag

---
+++ src/main/java/com/epam/student/krynytskyi/tag/LoginTag.java
@@ -0,0 +1,33 @@
+package com.epam.student.krynytskyi.tag;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import com.epam.student.krynytskyi.beans.LoginFormBean;
+import com.epam.student.krynytskyi.entity.User;
+
+public class LoginTag extends SimpleTagSupport{
+	@Override
+	public void doTag() throws JspException, IOException {
+		User user = (User) getJspContext().findAttribute("user");
+		LoginFormBean loginFormBean = (LoginFormBean) getJspContext().findAttribute("loginFormDate");
+		String email = loginFormBean == null? "":loginFormBean.getEmail();
+		if(user != null){
+			getJspContext().getOut().write("<h2>Name: "+user.getName()+"</h2>"+
+					"<form action=\"logOut\" method=\"post\">"+
+					"<input type=\"hidden\" name=\"id\" value=\""+ user.getId() +"\"/>"+
+					"<button type=\"submit\" class=\"btn btn-default\">log out</button>"+
+				"</form>");
+		}else{
+			getJspContext().getOut().write(
+						"<h2>Login to your account</h2>"+
+						"<form action=\"login\" method=\"post\">"+
+							"<input type=\"email\" name=\"email\" value=\""+ email +"\" placeholder=\"Email Address\"/>"+
+							"<input type=\"password\" name=\"password\" placeholder=\"password\"/>"+
+							"<button type=\"submit\" class=\"btn btn-default\">Login</button>"+
+						"</form>");
+		}
+	}
+}
--- src/main/webapp/WEB-INF/pages/login.jsp
+++ src/main/webapp/WEB-INF/pages/login.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ taglib prefix="reg" uri="http://koraytugay.com/capthcaTags" %>
+<%@ taglib prefix="reg" uri="http://koraytugay.com/loginTags" %>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,12 +40,7 @@
 			<div class="row">
 				<div class="col-sm-4 col-sm-offset-1">
 					<div class="login-form"><!--login form-->
-						<h2>Login to your account</h2>
-						<form action="login" method="post">
-							<input type="email" name="email" value="${loginFormDate.email}" placeholder="Email Address"/>
-							<input type="text" name="password" placeholder="password"/>
-							<button type="submit" class="btn btn-default">Login</button>
-						</form>
+						<reg:loginTags />
 					</div><!--/login form-->
 				</div>
 			</div>
