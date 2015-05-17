46882ca2f9353218281f1e1d205f440d3e206302
Anton Krynytskyi <antonkrinitskiy@gmail.com>
2015-05-17 18:19:19 +0300

    refactore CardController

--- src/main/java/com/epam/student/krynytskyi/controllers/CardController.java
+++ src/main/java/com/epam/student/krynytskyi/controllers/CardController.java
@@ -1,8 +1,6 @@
 package com.epam.student.krynytskyi.controllers;
 
-
 import com.epam.student.krynytskyi.beans.card.CardInfo;
-import com.epam.student.krynytskyi.beans.card.ProductFixedPrice;
 import com.epam.student.krynytskyi.containers.CardContainer;
 import com.epam.student.krynytskyi.entity.Product;
 import com.epam.student.krynytskyi.service.ProductService;
@@ -16,47 +14,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @WebServlet("/card")
 public class CardController extends HttpServlet {
 
     private static final Logger log = Logger.getLogger(CardController.class);
-    private JSONSerializer jsonSerializer = new JSONSerializer();
+    public static final String PRODUCT_SERVICE_PARAMETER = "productService";
+    public static final String PRODUCT_ID_PARAMETER = "id";
+    public static final String CARD_JSP_PATH = "/WEB-INF/pages/card.jsp";
+    public static final String CARD_INFO_SESSION_ATTRIBUTE = "cardInfo";
+    public static final String CARD_SESSION_ATTRIBUTE = "card";
+    private JSONSerializer jsonSerializer;
     private ProductService productService;
 
     @Override
     public void init() throws ServletException {
         productService = (ProductService) getServletContext().getAttribute("");
+        jsonSerializer = new JSONSerializer();
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/pages/card.jsp").forward(req, resp);
+        req.getRequestDispatcher(CARD_JSP_PATH).forward(req, resp);
     }
-
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ProductService productService = (ProductService) req.getServletContext().getAttribute(PRODUCT_SERVICE_PARAMETER);
+        CardContainer card = getCardContainer(req);
+        addProductToCard(req, productService, card);
+        CardInfo cardInfo = createCardInfo(card);
+        req.getSession().setAttribute(CARD_INFO_SESSION_ATTRIBUTE, cardInfo);
+        print(resp, cardInfo);
+    }
+
+    private CardContainer getCardContainer(HttpServletRequest req) {
         CardContainer card = (CardContainer) req.getSession().getAttribute("card");
-        ProductService productService = (ProductService) req.getServletContext().getAttribute("productService");
-        if(card == null){
+        if (card == null) {
             card = new CardContainer();
-            req.getSession().setAttribute("card",card);
-            log.debug("Card created.");
+            req.getSession().setAttribute(CARD_SESSION_ATTRIBUTE, card);
         }
-        String productId = req.getParameter("id");
+        return card;
+    }
+
+    private void addProductToCard(HttpServletRequest req, ProductService productService, CardContainer card) {
+        String productId = req.getParameter(PRODUCT_ID_PARAMETER);
         Product product = getProductById(productService, productId);
-        ProductFixedPrice one = new ProductFixedPrice(product);
-        ProductFixedPrice two = new ProductFixedPrice(product);
-        log.debug(one.equals(two) + " :: " + one.hashCode() + " || " + two.hashCode());
         card.add(product, 1);
+    }
+
+    private CardInfo createCardInfo(CardContainer card) {
         CardInfo cardInfo = new CardInfo();
         cardInfo.setProductsNumber(card.size());
         cardInfo.setTotalCost(card.calculationPurchases());
-
-        log.debug(productId + " :: card-size: " + card.size() + " items :: result price " + card.calculationPurchases());
-        req.getSession().setAttribute("cardInfo",cardInfo);
-        print(resp, cardInfo);
+        return cardInfo;
     }
 
     private Product getProductById(ProductService productService, String productId) {
@@ -64,7 +74,7 @@ public class CardController extends HttpServlet {
             return productService.getById(productId);
         } catch (Exception e) {
             log.error(e);
-            return  null;
+            return null;
         }
     }
 
